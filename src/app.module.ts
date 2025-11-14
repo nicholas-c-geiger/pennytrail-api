953093217d82ffd03ffdd6e2f3b1ad32611e144a
Nicholas Geiger <nicholas.c.geiger@outlook.com>
2025-11-14 11:59:48 -0800

    Added config files and used them instead of importing dotenv/config when needed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+    }),
+    PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
